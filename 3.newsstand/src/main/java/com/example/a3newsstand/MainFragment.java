ca33dee815246a31d3ab06321dfc4a38ec071ac5
comp <14masterblaster14@gmail.com>
2017-04-15 17:25:52 +0530

    ok

---
+++ 3.newsstand/src/main/java/com/example/a3newsstand/MainFragment.java
@@ -0,0 +1,56 @@
+package com.example.a3newsstand;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by comp on 3/23/2017.
+ */
+public class MainFragment extends Fragment {
+
+    private static final String TAG = MainFragment.class.getSimpleName();
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        final Toolbar toolbar = (Toolbar) view.findViewById(R.id.tool_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+
+        /* Do not show the title of the app in the toolbar */
+        activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+/*
+        TabLayout tabLayout = (TabLayout)view.findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_highlights));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_google));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_film));
+*/
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main, menu);
+    }
+
+}
